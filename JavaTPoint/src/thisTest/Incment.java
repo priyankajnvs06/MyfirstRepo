b6dac7e25f4f8d5482d5431f60bd0fe03af9ce08
QAP4 <QAP4@QAP4-PC>
2019-04-08 18:09:38 +0530

    545

---
+++ JavaTPoint/src/thisTest/Incment.java
@@ -0,0 +1,118 @@
+package thisTest;
+
+public class Incment 
+{
+	public static void main(String []args)
+		{ 
+	/*		 int x=10;
+			System.out.println(x);
+			System.out.println(x++);
+			System.out.println(x);
+			System.out.println(++x);
+			System.out.println(x);
+			System.out.println(x--);
+			System.out.println(--x);
+			//.............................
+			//to check given no is prime or not..........
+		   System.out.println("Enater a number");
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			System.out.println("number is="+n);
+			int flag=0;
+			for(int j=2;j<=n/2;j++)
+			  { 
+				if(n%j==0)
+				{flag=1; break;} 
+			  }
+			if(flag==1)
+			  
+				System.out.println("Number is not prime ");
+			  
+			else
+				System.out.println("Number is  prime ");
+				////////////////////////////////////////////////////////////////
+		//...............................................
+			int l=0,m=1,sum=0;
+			int i=0;
+		while(l<=100)
+		{
+			System.out.println(sum);
+			sum=l+m;
+			l=m;
+			m=sum;
+		}
+		
+		/////////////////////////////////////////////////////////////////////
+		///  *****
+		     *****
+		     *****
+		     *****
+		     *****   to print star pattern..............  
+		 
+		int n,o;
+		for(n=0;n<2;n++)
+		{
+			for(o=0;o<2;o++)
+			{
+				System.out.print("*");
+			}
+			o--;
+			System.out.println();
+		}
+		///////////////////////////////////////////////////////////////////////////
+		///................................................... * * * * *
+		                                                       * * * * *
+		int n,o;
+		int p=3;
+		for(n=0;n<p;n++)
+		{
+			for(o=0;o<p;o++)
+			{
+				System.out.print(" * ");
+			}
+			p--;
+			
+			System.out.print("\n");
+		}
+		////////////////////////////////////////////////////
+		int n,o;
+		int p=5;
+		for(n=0;n<5;n++)
+		{
+			for(o=0;o<p;o++)
+			{
+				System.out.print(" * ");
+			}
+			p--;
+			System.out.print("\n");
+		}
+		//////////////////////////////////////////////////////
+		int n,o;
+		int p=5;
+		for(n=0;n<5;n++)
+		{
+			for(o=0;o<n;o++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.print("\n");
+		}
+		}*/
+	//////////////////////////////////////////////////////////////
+		int n,o;
+		int p=5;
+		for(n=1;n<=5;n++)
+		{
+			for(o=1;o<=n;o++)
+			{
+				System.out.print(" 000000 ");
+			}
+			System.out.print("\n");
+		}
+		}
+
+
+	}
+
+
+
